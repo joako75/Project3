742f44894035bb934594960018b3470dc9f4008e
Joaquin Rojas Chang <60677017+joako75@users.noreply.github.com>
2020-02-23 14:44:45 -0500

    Add files via upload

---
+++ search/src/main/java/cop2805/OuterFrame.java
@@ -0,0 +1,31 @@
+package cop2805;
+
+import java.awt.*;
+
+//Class containing the outer frame
+public class OuterFrame{
+	OuterFrame(){
+//Constructs the frame and 3 button objects
+	Frame frame1 = new Frame( "Search Engine" );
+	Button searchButton = new Button( "Search" );
+	Button aboutButton = new Button ( "About" );
+	Button indexButton = new Button ( "Index" );
+
+//Sets position of the buttons and adds them to the frame
+	searchButton.setBounds(500, 50, 80, 30);
+	frame1.add(searchButton);
+	aboutButton.setBounds(500, 425, 80, 30);
+	frame1.add(aboutButton);
+	indexButton.setBounds(400, 425, 80, 30);
+	frame1.add(indexButton);
+	frame1.setSize(700,500);
+	frame1.setLayout(null);
+	frame1.setVisible(true);
+	}
+}
+
+//Need a class for the inner frame (index window)
+
+	//public static void main( String args[] ){
+		//OuterFrame frame1 = new OuterFrame();
+		//}
