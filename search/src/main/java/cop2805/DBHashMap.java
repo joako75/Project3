21377db28ce31c4430ed7e9b4a9397db8209ada7
abrown916 <abrown309@hawkmail.hccfl.edu>
2020-04-09 00:26:45 -0400

    Moved hashmap to DBHashMap class, added stub methods.

---
+++ search/src/main/java/cop2805/DBHashMap.java
@@ -0,0 +1,84 @@
+/*
+Placeholder/example for hashmap, gathered from 
+https://www.geeksforgeeks.org/map-interface-java-examples/ and edited to better fit the project.
+
+More method information available at
+https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
+
+Return values should not be 0, they are set to 0 temporarily to alleviate IDE warnings.
+*/
+
+package cop2805;
+
+import java.util.*; 
+class DBHashMap 
+{ 
+   public static void main(String args[]) 
+   { 
+       Map< String,String> hm =  
+                        new HashMap< String, String>(); 
+       hm.put("a", new String()); 
+       hm.put("b", new String()); 
+       hm.put("c", new String()); 
+       hm.put("d", new String()); 
+  
+       // Returns Set view      
+       Set< Map.Entry< String,String> > st = hm.entrySet();    
+  
+       for (Map.Entry< String,String> me:st) 
+       { 
+           System.out.print(me.getKey()+":"); 
+           System.out.println(me.getValue()); 
+       } 
+   }
+   
+   //Associates the specified value with the specified key in this map.
+   public Object put(Object key, Object value) {
+	   
+	   return 0;
+	   
+   }
+   
+   //Copies all of the mappings from the specified map to this map.
+   public void putAll(Map map) {
+	   
+	   return 0;
+   }
+   
+   //Removes mapping for specified key, would be used for the "Remove" button I believe.
+   public Object remove(Object key) {
+	   
+	   return 0;
+   }
+   
+   /*
+   Returns the value to which the specified key is mapped, or null if this map contains no
+   mapping for the key.
+   */
+   public Object get(Object key) {
+	   
+	   return 0;
+	   
+   }
+   
+   //Returns true if this map contains a mapping for the specified key.
+   public boolean containsKey(Object key) {
+	   
+	   return 0;
+	   
+   }
+   
+   //Returns a Set view of the keys contained in this map.
+   public Set keySet() {
+	   
+	   return 0;
+	   
+   }
+   
+   //Returns a Set view of the mappings contained in this map.
+   public Set entrySet() {
+	   
+	   return 0;
+	   
+   }
+} 
--- search/src/main/java/cop2805/DBModel.java
+++ search/src/main/java/cop2805/DBModel.java
@@ -39,25 +39,4 @@ public DBModel(){
     public String getColumnName(int col){
     return columnName[col];
     }
-    
-    //Placeholder/example for hashmap, gathered from https://www.geeksforgeeks.org/map-interface-java-examples/
-    //Not sure if this belongs in a different class.
-    public static void main(String args[]) 
-    { 
-        Map< String,Integer> hm =  
-                         new HashMap< String,Integer>(); 
-        hm.put("a", new Integer(100)); 
-        hm.put("b", new Integer(200)); 
-        hm.put("c", new Integer(300)); 
-        hm.put("d", new Integer(400)); 
-   
-        // Returns Set view      
-        Set< Map.Entry< String,Integer> > st = hm.entrySet();    
-   
-        for (Map.Entry< String,Integer> me:st) 
-        { 
-            System.out.print(me.getKey()+":"); 
-            System.out.println(me.getValue()); 
-        } 
-    } 
 }
